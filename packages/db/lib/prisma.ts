d111ede51b3587b666710a70ad325f12a4b8b082
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-09-20 22:25:49 -0600

    :recycle: Fix vercel prisma gen / turbo cache

--- packages/db/lib/prisma.ts
+++ packages/db/lib/prisma.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line -- need to import the generated code
-import {PrismaClient} from './generated/client'
+import { PrismaClient } from "@db/lib/generated/client";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -7,11 +7,14 @@ import {PrismaClient} from './generated/client'
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-const globalForPrisma: { prisma?: PrismaClient } = global as unknown as { prisma: PrismaClient }
+const globalForPrisma: { prisma?: PrismaClient } = global as unknown as {
+  prisma: PrismaClient;
+};
 
-export const prisma: PrismaClient = globalForPrisma.prisma || new PrismaClient()
+export const prisma: PrismaClient =
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // eslint-disable-next-line import/no-default-export -- need to get dev mode working correctly
-export default prisma
+export default prisma;
