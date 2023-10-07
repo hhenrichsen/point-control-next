19bb3224eb228f1bef9e66a0a7e214c4b1ed3147
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-10-07 12:19:14 -0600

    :test_tube: Add testing framework

---
+++ apps/web/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Home from "../pages/index";
+
+test("Should have a heading", () => {
+  render(<Home />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "PointControl" }),
+  ).toBeDefined();
+});
