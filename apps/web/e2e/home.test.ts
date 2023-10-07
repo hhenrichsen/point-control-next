6c5ec24deb69a1128fe947a730e409f332a2c690
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-10-07 12:19:14 -0600

    :test_tube: Add testing framework

---
+++ apps/web/e2e/home.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "@playwright/test";
+
+test("should navigate the 404 page when clicking my invalid link", async ({
+  page,
+}) => {
+  await page.goto("/");
+  await expect(page).toHaveScreenshot("home.png");
+  await page.click("text=Game List");
+  await expect(page).toHaveURL("/games");
+  await expect(page.locator("h1")).toContainText("404");
+  await expect(page).toHaveScreenshot("404.png");
+});
---
+++ apps/web/playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "e2e",
+  reporter: "html",
+  use: {
+    baseURL: "http://127.0.0.1:3000",
+    screenshot: "only-on-failure",
+  },
+  // Run your local dev server before starting the tests
+  webServer: {
+    command: "pnpm run start",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+    stdout: "ignore",
+    stderr: "pipe",
+  },
+});
