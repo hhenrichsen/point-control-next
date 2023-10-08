e9976dbd2c423839a89426707265feada8d87531
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-10-08 02:07:08 -0600

    :fire: Remove prettier import order

--- tooling/prettier-config/index.mjs
+++ tooling/prettier-config/index.mjs
@@ -1,30 +1,14 @@
 import { fileURLToPath } from "url";
 
 /** @typedef  {import("prettier").Config} PrettierConfig */
-/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-prisma",
-    "prettier-plugin-tailwindcss",
-  ],
+  plugins: ["prettier-plugin-prisma", "prettier-plugin-tailwindcss"],
   tailwindConfig: fileURLToPath(
     new URL("../tailwind-config/index.ts", import.meta.url),
   ),
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "^@pointcontrol/",
-    "^~/",
-    "^[../]",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
 };
 
 export default config;
