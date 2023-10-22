774babeb3e0bad946ceb99b601cf7c6699c61419
Hunter <hunter.henrichsen@gmail.com>
2023-10-22 17:41:48 -0600

    :white_check_mark: Update UI tests

--- apps/web/e2e/home.test.ts
+++ apps/web/e2e/home.test.ts
@@ -4,9 +4,11 @@ test("should navigate the 404 page when clicking my invalid link", async ({
   page,
 }) => {
   await page.goto("/");
+  await page.waitForSelector("button");
   await expect(page).toHaveScreenshot("home.png");
-  await page.locator("Button").click();
+  await page.locator("a[href='/games'] button").click();
   await expect(page).toHaveURL("/games");
+  await page.waitForLoadState();
   await expect(page.locator("h1")).toContainText("404");
   await expect(page).toHaveScreenshot("404.png");
 });
